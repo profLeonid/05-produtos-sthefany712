b49ffa096ab0df424b51f1a5ef163f18f454e6e7
STHEFANY DA SILVA CARMO CORREIA <25203734@dsn127macprof.cfp127aluno.local>
2026-02-24 11:32:47 -0300

    Atividade Produto

--- main.js
+++ main.js
@@ -9,4 +9,29 @@ function adicionarProduto() {
     span.className = 'flex flex-col bg-yellow-100 px-8 py-2'
 
     lista.appendChild(span)
+    
+}
+
+function adicionarCodigo() {
+    const lista = document.getElementById('lista')
+    const codigo = document.getElementById('codigo')
+
+    const span = document.createElement('span')
+    span.textContent = codigo.value
+    span.className = 'flex flex-col bg-yellow-100 px-8 py-2'
+
+    lista.appendChild(span)
+    
+}
+
+function adicionarQuantidade() {
+    const lista = document.getElementById('lista')
+    const produto = document.getElementById('quantia')
+
+    const span = document.createElement('span')
+    span.textContent = quantidade.value
+    span.className = 'flex flex-col bg-yellow-100 px-8 py-2'
+
+    lista.appendChild(span)
+    
 }
