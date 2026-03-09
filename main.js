167fb9aab5a036d007a5b076cdc3a149669b5487
STHEFANY DA SILVA CARMO CORREIA <25203734@CFP127ALUNO.LOCAL>
2026-03-09 09:40:39 -0300

    atividade

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function adicionarProduto() {
     const produto = document.getElementById('produto') //id desse objt produto foi criado no html
     const codigo = document.getElementById('codigo')
     const quantidade = document.getElementById('quantidade')
-
+    
     // Chama a funçao para cada input separadamente
     criarCaixa(codigo.value)
     criarCaixa(produto.value)
@@ -23,5 +23,13 @@ function adicionarProduto() {
         novoSpan.textContent = valor
         novoSpan.className = 'bg-yellow-100 px-8 py-2 border border-yellow-200 rounded shadow-sm'
         lista.appendChild(novoSpan) //lista é o pai e o span é o filho
+    }
+}
+
+function apagarProduto() {
+    const lista = document.getElementById('lista')
+
+    if(lista.lastElementChild){
+        lista.lastElementChild.remove()
     }
 }
